0f49bf740212e0546d5495f1072e78a5840da229
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2018-09-19 20:17:51 -0500

    commit inicial

---
+++ front/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      estadoPrueba:''
+    }
+  }
+
+  componentDidMount(){
+    fetch("/api?id=0")
+    .then((res)=>{
+      return res.json();
+    })
+    .then((prueba)=>{
+      console.log(prueba);
+      this.setState({ estadoPrueba: prueba })
+    })
+    .catch((err) =>{console.log(err)});
+  }
+  render() {
+    let x = this.state.estadoPrueba;
+    if(x){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Parcial 1 Pablo Alvarado</h1>
+        </header>
+          
+           <Row>
+            <Col sm="12" md={{ size: 8, offset: 2 }}>
+              <Card body outline color="danger">
+                <CardTitle>{x.name}</CardTitle>
+                <CardText>{x.texto}</CardText>
+                <Button>Go somewhere</Button>
+              </Card>
+            </Col>
+          </Row>
+          <br/>
+      </div>
+    );  
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Parcial 1 Pablo Alvarado</h1>
+        </header>
+        <h2>.....Cargando........</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+"use strict";
+require("dotenv").config();
+var express = require('express');
+var router = new express.Router();
+const MongoClient = require("mongodb").MongoClient;
+const url = process.env.MLAB;
+
+
+router.get('/', function(req, res) {
+	console.log("llega");
+	let id=parseInt(req.query.id);
+	const p = {id: id };
+	console.log(p);
+	getPrueba(p, (prueba)=> res.send(prueba));
+});
+function findPrueba (query, db, callback) {
+  const collection = db.collection("demo");
+  collection.findOne(query, (err, docs) => {
+    if(err){console.log("no se encontro")} //se revisan que no se den errores.
+    console.log("Found " + JSON.stringify(docs) + " urls");
+    if (docs === null || docs === undefined) docs = { error: "no se encontro el item de prueba" }; //sino se encuentra el usuario, se responde con un error
+    callback(docs); //se responde con el usuario
+  });
+}
+function getPrueba (query, callback){
+	MongoClient.connect(url, (err, client) => { // conexion a la base de datos
+	if(err){console.log("Problemas con la conexion")}
+	const db = client.db("parcial_1"); //se pide la collecion
+	findPrueba(query, db, callback); //se busca el item en la base de datos	
+	client.close(); //se cierra collecion
+	});
+}
+module.exports = router;
